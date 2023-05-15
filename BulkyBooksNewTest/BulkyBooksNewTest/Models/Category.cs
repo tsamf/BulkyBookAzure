bd76ba7e64d0ba3f08c315f72e4bb5986ae35d15
TravisSamford <samfordt@gmail.com>
2023-05-14 20:17:27 -0500

    Added project to github which includes entitiy framework and Sql integration

---
+++ BulkyBooksNewTest/BulkyBooksNewTest/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis.Operations;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBooksNewTest.Models
+{
+    public class Category
+    {
+        [Key]
+        [Required]
+        public int id { get; set; }
+
+        public string? Fname { get; set; }
+
+        public string? Lname { get; set; }
+
+        [Required]
+        public DateTime createdDateTime { get; set; } = DateTime.Now;
+    }
+}
---
+++ BulkyBooksNewTest/BulkyBooksNewTest/data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BulkyBooksNewTest.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBooksNewTest.data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
